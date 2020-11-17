3767780cf61debbdee7595834ef5bffd821c41c9
ip11 <iprash011@gmail.com>
2020-11-17 07:56:58 +0530

    External CSS

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Fragment } from 'react';
 
+import './style.css';
 
 class App extends React.Component {
     render() {
         return (
             <Fragment>
-                <div id="sample1" style={style1}>
+                <div id="sample1" >
                 </div>
                 <br />
-                <div id="sample2" style={style2}>
+                <div id="sample2" >
                 </div>
                 <h1>Hello {Math.sqrt(625)} </h1>
                 <h2>There</h2>
@@ -17,17 +18,6 @@ class App extends React.Component {
     }
 }
 
-const style1 = {
-    backgroundColor: 'red',
-    height: '100px',
-    width: '100px'
-}
-
-const style2 = {
-    backgroundColor: 'blue',
-    height: '100px',
-    width: '100px'
-}
 
 
 export default App;
