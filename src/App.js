4e73cd80f1c8b9b90bcc17657bc5303dd7d2072e
ip11 <iprash011@gmail.com>
2020-11-19 08:48:02 +0530

    Loading Component - Conditional Rendering

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 import React from 'react';
-
+import axios from 'axios';
 import './App.css';
-
 import NavBar from './components/NavBar';
 import Users from './components/Users';
 
 class App extends React.Component {
+    state = {
+        users: [],
+        loading: false
+    }
+    async componentDidMount() {
+        this.setState({
+            loading: true
+        });
+        const res = await axios.get("https://api.github.com/users");
+        this.setState({
+            users: res.data,
+            loading: false
+        });
+
+    }
     render() {
         return (
             <div>
                 <NavBar />
                 <div className="container">
-                    <Users />
+                    <Users users={this.state.users} loading={this.state.loading} />
+
                 </div>
             </div>
         )
     }
+
 }
 
 
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,53 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import UserItem from './UserItem';
+import Loading from './Loading';
 
-
-class Users extends Component {
-    state = {
-        users: [
-            {
-                id: '1',
-                login: 'ip11',
-                avatar_url: 'https://avatars0.githubusercontent.com/u/39377668?v=4',
-                html_url: 'https://github.com/ip11'
-            },
-            {
-                id: "2",
-                login: "anilraje122",
-                avatar_url: "https://avatars2.githubusercontent.com/u/26355963?v=4",
-                html_url: "https://github.com/anilraje122",
-            },
-            {
-                id: "3",
-                login: "sharu24",
-                avatar_url: "https://avatars0.githubusercontent.com/u/29565360?v=4",
-                html_url: "https://github.com/Sharu24"
-            },
-            {
-                id: '4',
-                login: 'aditya278',
-                avatar_url: 'https://avatars2.githubusercontent.com/u/25431607?v=4',
-                html_url: "https://github.com/aditya278"
-            },
-            {
-                id: '5',
-                login: 'pahalsonu',
-                avatar_url: "https://avatars1.githubusercontent.com/u/68494713?v=4",
-                html_url: "https://github.com/pahalsonu"
-            }
-        ]
-    }
-    render() {
-        return (
-            <div style={userStyle}>
-                { this.state.users.map(user => (
-                    <UserItem key={user.id}
-                        user={user}
-                    />
-                ))}
-            </div>
-        )
+const Users = (props) => {
+    if (props.loading) {
+        return <Loading />;
+    } else {
+        return <div style={userStyle}>
+            {props.users.map((user) => (
+                <UserItem key={user.id} user={user} />
+            ))}
+        </div>
     }
 }
 
