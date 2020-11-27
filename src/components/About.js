6b7fbd784a2618f0f631c6c440a0c3711908f5c0
ip11 <iprash011@gmail.com>
2020-11-27 23:24:18 +0530

    React Forms

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,54 @@
-import React, { Fragment } from 'react'
+import React, { Component } from 'react'
 
-function About() {
-    return (
-        <Fragment>
-            <h1>About GitHub Search Engine</h1>
-            <p> This app is built using React Technology</p>
-        </Fragment>
-    )
+class About extends Component {
+    constructor() {
+        super();
+        this.state = {
+            fname: '',
+            lname: '',
+            password: '',
+            gender: ''
+        }
+    }
+    onSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state);
+    }
+    onChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+        //Computed Property Names in Objects
+        //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Object_initializer#Computed_property_names
+        // console.log(e.target.name, e.target.value);
+    }
+    render() {
+        return (
+            <>
+                <h1>React Forms</h1>
+                <p>This is just some form submission in React and Express</p>
+                <div className="container" style={{ margin: '10px 10px' }}>
+                    <form onSubmit={this.onSubmit}>
+                        <label htmlFor="fname">First name:</label>
+                        <input type="text" name="fname" value={this.state.fname} onChange={this.onChange} />
+                        <label htmlFor="lname">Last name:</label>
+                        <input type="text" name="lname" value={this.state.lname} onChange={this.onChange} />
+                        <label htmlFor="password">Password:</label>
+                        <input type="password" name="password" value={this.state.password} onChange={this.onChange} />
+
+                        <input type="radio" id="male" name="gender" value="male" onChange={this.onChange}/>
+                        <label htmlFor="male"> Male </label><br />
+                        <input type="radio" id="female" name="gender" value="female" onChange={this.onChange}/>
+                        <label htmlFor="female"> Female </label><br />
+
+
+                        <input type="submit" value="Submit"></input>
+                    </form>
+                </div>
+
+            </>
+        )
+    }
 }
 
 export default About;
