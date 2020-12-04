f626a663022c29cd143eda4dad35dcb524b4ae63
Anil Raj <anilraje122@gmail.com>
2020-12-04 20:01:30 +0530

    Add context api for Users

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,32 +1,30 @@
-import React from 'react';
+import React, { useContext } from "react";
 
-import UserItem from './UserItem';
-import Loading from './Loading';
-import PropTypes from 'prop-types';
+import UserItem from "./UserItem";
+import Loading from "./Loading";
+import githubContext from "../context/GitHub/githubContext";
 
+function Users() {
+  const GithubContext = useContext(githubContext);
+  const { users, loading } = GithubContext;
 
-function Users({ users, loading }) {
-    if (loading) {
-        return <Loading />;
-    } else {
-        return <div style={userStyle}>
-            {users.map((user) => (
-                <UserItem key={user.id} user={user} />
-            ))}
-        </div>
-    }
+  if (loading) {
+    return <Loading />;
+  } else {
+    return (
+      <div style={userStyle}>
+        {users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  }
 }
 
 const userStyle = {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(3,1fr)',
-    gridGap: '1rem'
-}
-
-Users.propTypes = {
-    users : PropTypes.array.isRequired,
-    loading : PropTypes.bool.isRequired
-}
-
+  display: "grid",
+  gridTemplateColumns: "repeat(3,1fr)",
+  gridGap: "1rem",
+};
 
 export default Users;
